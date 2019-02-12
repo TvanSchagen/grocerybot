5d036f1031ec83a7b1bc4005cc647383f2398f69
Teun van Schagen <t.vanschagen1996@gmail.com>
2019-02-12 19:34:10 +0100

    feat(): added COOP spider and added download delay in settings

--- grocerybot/spiders/coop_spider.py
+++ grocerybot/spiders/coop_spider.py
@@ -2,23 +2,23 @@ import scrapy
 
 
 class ProductsSpider(scrapy.Spider):
-    name = 'ah'
-    start_urls = ['https://www.ah.nl/producten']
+    name = 'coop'
+    start_urls = ['https://www.coop.nl/boodschappen']
 
     def parse(self, response):
         self.logger.info('main: %s' % response.url)
-        for href in response.css('div.product-category-navigation-lane').css('a::attr(href)'):
+        for href in response.css('li.categoryItem').css('a::attr(href)'):
             yield response.follow(href, self.parse_categories)
 
     def parse_categories(self, response):
         self.logger.info('cat: %s' % response.url)
-        for href in response.css('article.product').css('a::attr(href)'):
+        for href in response.css('div.formfields').css('a::attr(href)'):
             yield response.follow(href, self.parse_products)
 
     def parse_products(self, response):
         self.logger.info('product: %s' % response.url)
-        page = response.url.split("/")[-1]
-        filename = 'data/ah/%s.html' % page
+        title = response.css('h1.head1::text').get()
+        filename = 'data/coop/%s.html' % title
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.logger.info('Saved file %s' % filename)
