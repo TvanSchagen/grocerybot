ae5055752ed5b49a6766aae1415ad228d6825642
Teun van Schagen <t.vanschagen1996@gmail.com>
2019-02-13 17:24:29 +0100

    feat(): add coop pagination crawling; parallel spider running

--- grocerybot/spiders/coop_spider.py
+++ grocerybot/spiders/coop_spider.py
@@ -5,19 +5,28 @@ class ProductsSpider(scrapy.Spider):
     name = 'coop'
     start_urls = ['https://www.coop.nl/boodschappen']
 
+    # start by crawling all pages from the start url, detect categories (that lead to overview pages) and parse them
     def parse(self, response):
         self.logger.info('main: %s' % response.url)
         for href in response.css('li.categoryItem').css('a::attr(href)'):
-            yield response.follow(href, self.parse_categories)
+            yield response.follow(href, self.parse_overview_pages)
 
-    def parse_categories(self, response):
+    # then parse the overview pages that follow from categories, and overview pages that follow from pagination within categories
+    def parse_overview_pages(self, response):
         self.logger.info('cat: %s' % response.url)
+        # detect individual product pages and parse them
         for href in response.css('div.formfields').css('a::attr(href)'):
             yield response.follow(href, self.parse_products)
+        # detect pagination links and parse them
+        for href in response.css('li.pagination__listItem').css('a::attr(href)'):
+            yield response.follow(href, self.parse_overview_pages)
 
+    # parse products and write each product to a file
     def parse_products(self, response):
         self.logger.info('product: %s' % response.url)
+        # extract the title from the product
         title = response.css('h1.head1::text').get()
+        # save the HTML with the extracted title
         filename = 'data/coop/%s.html' % title
         with open(filename, 'wb') as f:
             f.write(response.body)
