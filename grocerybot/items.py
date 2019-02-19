52eeafadd2e50e956533408ca4af3b4197fc9251
Dimitris Michailidis <jimichailidis@gmail.com>
2019-02-19 15:26:01 +0100

    GrocerybotItem contains all fields

--- grocerybot/items.py
+++ grocerybot/items.py
@@ -6,37 +6,38 @@ import string
 import scrapy
 
 
-def create_grocery_bot_item(title: string, url: string, filepath: string, timestamp: datetime):
+def create_grocery_bot_item(
+        product_name: string, page_title: string, description: string,
+        supermarket: string, url: string, date: datetime, weight: string,
+        size: string, category: string):
     """
     Creates a GrocerybotItem object, assigns it's fields and returns it.
-    :param title: title of the parsed product
-    :param url: remote url location of the product
-    :param filepath: local path of the saved page
-    :param timestamp: date & time when it was crawled
-    :return: GroverybotItem with all the fields assigned
     """
     item = GrocerybotItem()
 
-    item['title'] = title
+    item['product_name'] = product_name
+    item['page_title'] = page_title
+    item['description'] = description
+    item['supermarket'] = supermarket
     item['url'] = url
-    item['filepath'] = filepath
-    item['timestamp'] = timestamp.strftime("%d-%m-%Y %H:%M:%S")
+    item['date'] = date.strftime("%d-%m-%Y %H:%M:%S")
+    item['weight'] = weight
+    item['size'] = size
+    item['category'] = category
 
     return item
 
 
 class GrocerybotItem(scrapy.Item):
     """Attributes that accompany every scraped page. Used for easy indexing.
-
-    Attributes:
-        id (int): universal document id (used throughout the mined documents)
-        url (str): the remote url of the file in the supermarket's webpage.
-        filepath (str): the path of the file in the disk.
-        timestamp (datetime): the timestamp when the page was crawled.
     """
 
-    id = scrapy.Field()
-    title = scrapy.Field()
+    product_name = scrapy.Field()
+    page_title = scrapy.Field()
+    description = scrapy.Field()
+    supermarket = scrapy.Field()
     url = scrapy.Field()
-    filepath = scrapy.Field()
-    timestamp = scrapy.Field()
+    date = scrapy.Field()
+    weight = scrapy.Field()
+    size = scrapy.Field()
+    category = scrapy.Field()
