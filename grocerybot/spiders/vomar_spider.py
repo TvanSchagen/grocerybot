1caf3e167437b17716f1c2510e71218c1dd80713
Dimitris Michailidis <jimichailidis@gmail.com>
2019-02-20 16:24:42 +0100

    updated vomar spider with new fields

--- grocerybot/spiders/vomar_spider.py
+++ grocerybot/spiders/vomar_spider.py
@@ -30,15 +30,21 @@ class VomarSpider(scrapy.Spider):
 
     def parse_product(self, response):
         title = response.css('div.container div.container h1::text').get()
-        # page_title = response.css('title::text').get()
-        # page_title = page_title.replace('/', '')
-        # page_title = page_title.replace('_', '')
+        page_title = response.css('title::text').get()
+        page_title = page_title.replace('/', '')
+        page_title = page_title.replace('_', '')
+        description = response.css('div.container div.container div.productInfo div.col-md-6')[0].css('p::text')[0].get()
+        weight = response.css('div.container div.container div.productInfo div.col-md-6')[0].css('p::text')[1].get()
+        weight = weight.strip()
+        category = ' '.join(response.css('ol.breadcrumb li ::text').getall())
+        price = ''.join(response.css('div.priceUnitQuantity div.price span::text').getall())
 
-        filename = 'data/vomar/vomar-%s.html' % title
-        try:
-            with open(filename, 'wb') as f:
-                f.write(response.body)
-        except:
-            return
+        # filename = 'data/vomar/vomar-%s.html' % title
+        # try:
+        #     with open(filename, 'wb') as f:
+        #         f.write(response.body)
+        # except:
+        #     return
 
-        yield create_grocery_bot_item(title, response.url, filename, dt.now())
+        yield create_grocery_bot_item(title, page_title, description, 'vomar',
+                                      response.url, dt.now(), weight, '', category, price)
