033762d72a8fbcd32912593a825cccc0550a09eb
Dimitris Michailidis <jimichailidis@gmail.com>
2019-02-22 17:38:12 +0100

    multi_threaded_indexer now uses the Product class from product_indexer

--- back-end/multi_threaded_indexer.py
+++ back-end/multi_threaded_indexer.py
@@ -1,70 +1,9 @@
 import json
 import os
-import sys
-from datetime import datetime
 from multiprocessing import Process
+from elasticsearch_dsl import connections
+from grocerybot.indexers.product_indexer import Product
 
-from elasticsearch_dsl import Document, Date, Text, connections, Float
-
-class Product(Document):
-    product_name = Text()
-    page_title = Text()
-    description = Text()
-    supermarket = Text()
-    url = Text()
-    date = Date()
-    weight = Text()
-    size = Text()
-    category = Text()
-    price = Float()
-
-    class Index:
-        name = 'product'
-        settings = {
-            "analysis": {
-                "filter": {
-                    "dutch_stop": {
-                        "type": "stop",
-                        "stopwords": "_dutch_"
-                    },
-                    "dutch_keywords": {
-                        "type": "keyword_marker",
-                        "keywords": ["voorbeeld"]
-                    },
-                    "dutch_stemmer": {
-                        "type": "stemmer",
-                        "language": "dutch"
-                    },
-                    "dutch_override": {
-                        "type": "stemmer_override",
-                        "rules": [
-                            "fiets=>fiets",
-                            "bromfiets=>bromfiets",
-                            "ei=>eier",
-                            "kind=>kinder"
-                        ]
-                    }
-                },
-                "analyzer": {
-                    "rebuilt_dutch": {
-                        "tokenizer": "standard",
-                        "filter": [
-                            "lowercase",
-                            "dutch_stop",
-                            "dutch_keywords",
-                            "dutch_override",
-                            "dutch_stemmer"
-                        ]
-                    }
-                }
-            }
-        }
-
-    def save(self, **kwargs):
-        return super(Product, self).save(**kwargs)
-
-    def is_published(self):
-        return datetime.now() > self.date
 
 def start_indexer(output_file_name):
     # connect to es instance
@@ -81,15 +20,15 @@ def start_indexer(output_file_name):
         for product_json in json_array:
             # create and save the product
             product = Product(product_name=product_json['product_name'],
-                                page_title=product_json['page_title'],
-                                description=product_json['description'],
-                                supermarket=product_json['supermarket'],
-                                url=product_json['url'],
-                                date=product_json['date'],
-                                weight=product_json['weight'],
-                                size=product_json['size'],
-                                category=product_json['category'],
-                                price=product_json['price'])
+                              page_title=product_json['page_title'],
+                              description=product_json['description'],
+                              supermarket=product_json['supermarket'],
+                              url=product_json['url'],
+                              date=product_json['date'],
+                              weight=product_json['weight'],
+                              size=product_json['size'],
+                              category=product_json['category'],
+                              price=product_json['price'])
             product.save()
             print("saved " + product_json['url'])
 
