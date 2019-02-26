6c7771bf1ac4362678008b1533808f62fa1ba1a5
Teun van Schagen <t.vanschagen1996@gmail.com>
2019-02-26 15:01:38 +0100

    feat(backend): crawler now outputs json files

--- back-end/multi_threaded_crawler.py
+++ back-end/multi_threaded_crawler.py
@@ -2,13 +2,22 @@ from multiprocessing import Process
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.log import configure_logging
+import datetime
 
 
 def start_crawler(spider_name):
     # load logging settings
     configure_logging()
     # create a CrawlerProcess that starts a Twisted reactor to asyncronously execute the crawlers
-    process = CrawlerProcess(get_project_settings())
+    settings = get_project_settings()
+    # set output format to json
+    settings.set('FEED_FORMAT', 'JSON')
+    # create current date in string
+    now = datetime.datetime.now()
+    current_date = (str(now.day) + "-" + str(now.month) + "-" + str(now.year))
+    # set output type to file
+    settings.set('FEED_URI', current_date + "_" + spider_name + '.json')
+    process = CrawlerProcess(settings)
     process.crawl(spider_name)
     process.start()
 
@@ -16,7 +25,6 @@ def start_crawler(spider_name):
 if __name__ == "__main__":
     Process(target=start_crawler, args=('ah',)).start()
     Process(target=start_crawler, args=('vomar',)).start()
-    # Process(target=start_crawler, args=('lidl',)).start()
     Process(target=start_crawler, args=('coop',)).start()
     Process(target=start_crawler, args=('jumbo_products',)).start()
     Process(target=start_crawler, args=('plus_products',)).start()
